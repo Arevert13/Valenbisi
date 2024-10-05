867911f250d0fcecd3a32a8c134cf31595d3d6a5
AntoniBI <mollantoni02@gmail.com>
2024-10-05 17:28:56 +0200

    Update codigo

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 
 API_URL="https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20"
 OUTPUT_FILE="data.json"
+Historico="historico.txt"
 
 curl -s $API_URL -o $OUTPUT_FILE
 
@@ -9,12 +10,10 @@ while true
 do
 echo "curl -s $API_URL -o $OUTPUT_FILE"
 
-sleep 5
-done
-
-Historico="historico.txt"
 FECHA=$(date '+%Y-%m-%d %H:%M:%S')
 cat $OUTPUT_FILE | jq -c '.records[] | .record.fields' | while read -r estacion; do
 NOMBRE_ESTACION=$(echo $estacion | jq -r '.address')
 BICIS_DISPONIBLES=$(echo $estacion | jq -r '.available')
 echo "$FECHA, Estación: $NOMBRE_ESTACION, Bicis Disponibles: $BICIS_DISPONIBLES" >> $Historico
+sleep 5
+done
