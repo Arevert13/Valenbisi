ddc470d546ff7309f665b1a208e2fe192f2a672e
migueltocu <mitorrescuello@gmail.com>
2024-10-05 13:19:11 +0200

    descarga datos + json

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 API_URL="https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20"
-curl -s $API_URL
+OUTPUT_FILE="data.json"
+
+curl -s $API_URL -o $OUTPUT_FILE
