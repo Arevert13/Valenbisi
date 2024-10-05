b11bd5bfcfc42a74aba1c6a88b84dc5d0087ac0b
AntoniBI <mollantoni02@gmail.com>
2024-10-05 13:26:33 +0200

    Ejecutar codigo cada 5 segundos

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+API_URL="https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20"
+OUTPUT_FILE="data.json"
+
+curl -s $API_URL -o $OUTPUT_FILE
+
 while true
 do
-echo 
+echo "curl -s $API_URL -o $OUTPUT_FILE"
 
 sleep 5
 done
